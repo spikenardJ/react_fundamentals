72aaf7f9a3fbd6002d789ef8fe3b32c1a7c88ed6
spikenardJ <jennifer_annette@hotmail.com>
2024-12-31 22:51:59 -0600

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import "../PortfolioStyles.css";
+
+const About = () => {
+    return (
+            <section>
+                <h2>About Me</h2>
+                <p>I am a passionate Web Developer with a love for creating dynamic and user-friendly websites. <br/><br/>Welcome to my portfolio!</p>
+                <br/>
+                <p><small>Proficient in Python, HTML, CSS, JavaScript, Bootstrap, and SQL.</small></p>
+            </section>
+    );
+};
+    
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import "../PortfolioStyles.css";
+
+class Contact extends Component {
+    render () {
+        return (
+            <footer>
+                <h2>Contact</h2>
+                <p>Email: jennifer_annette@hotmail.com</p>
+                <p>LinkedIn: https://www.linkedin.com/in/jenniferhurst/</p>
+            </footer>
+        );
+    }
+}
+
+export default Contact;
